077bc542d52db7942f7c95deaef78ecad4c9ad48
Mohamed LOTF <mohamed.lotf@outlook.com>
2020-09-28 23:54:50 +0200

    add all files

---
+++ index.js
@@ -0,0 +1,129 @@
+const axios = require('axios')
+const express = require('express')
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+ 
+app.get('/users', async (req, res) => {
+
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/users")
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/users/"+req.params.id)
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.post('/users', async (req, res) => {
+    const newUser=req.body
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+        res.status(400).send({ error:true, message: 'Please provide all required field' });
+    }
+    const {data}= await axios.post("https://jsonplaceholder.typicode.com/users/", newUser)
+    console.log(newUser);
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    const updatedUser=req.body
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+        res.status(400).send({ error:true, message: 'Please provide all required field' });
+    }
+    const {data}= await axios.put("https://jsonplaceholder.typicode.com/users/"+req.params.id, updatedUser)
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const {data}= await axios.delete("https://jsonplaceholder.typicode.com/users/"+req.params.id)
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.get('/users/:id/posts', async (req, res) => {
+
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/posts?userId="+req.params.id)
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.get('/users/:id/albums', async (req, res) => {
+
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/albums?userId="+req.params.id)
+    try {
+        console.log(data);
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.get('/users/albums/photos',async (req,res)=>{
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/users")
+    try{
+        let users=data;
+        users.forEach(async user => {
+            const {data}= await axios.get("https://jsonplaceholder.typicode.com/albums?userId="+user.id)
+            try {
+                let userAlbums=data;
+                userAlbums.forEach(async uAlb => {
+                    const {data}= await axios.get("https://jsonplaceholder.typicode.com/albums/"+uAlb.id+"/photos")
+                    try {
+                        let photosInAlb=data;
+                        uAlb.Photos=photosInAlb;
+                    } catch (error) {
+                        console.log(error)
+                        res.send(error)   
+                    }
+                });
+                user.Albums=userAlbums;
+            } catch (error) {
+                console.log(error)
+                res.send(error)   
+            }
+        });
+        res.send(users)
+    }catch (error) {
+        console.log(error)
+        res.send(error)   
+    }
+})
+
+app.listen(3000)
